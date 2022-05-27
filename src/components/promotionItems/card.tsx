e760f16bdd34b3b84ee9d2879b7f7156e9debcb5
guhhh25 <guhhh25@gmail.com>
2022-05-27 20:13:28 -0300

    update

--- src/components/promotionItems/card.tsx
+++ src/components/promotionItems/card.tsx
@@ -7,13 +7,13 @@ import Button from '@mui/material/Button';
 
 export default function Card(props:any){
   return(
-    <div style={{display: 'flex', flexDirection: 'column',  backgroundColor: '#565656', borderRadius:'20px', boxShadow:'7px 3px 15px 0px rgba(0,0,0,1)'}}> 
+    <div style={{display: 'flex', width: '250px', flexDirection: 'column',  backgroundColor: '#565656', borderRadius:'20px', boxShadow:'7px 3px 15px 0px rgba(0,0,0,0.3)'}}> 
     <div style={{backgroundColor:'#242424', display:'flex', alignItems: 'center', justifyContent: 'center', width:'100%'}}>
         <Image src={props.image} height={190} width={190}/>
         </div>
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'20px', fontSize:'30px'}}>{props.title}</div>
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'20px', fontSize:'20px'}}>{props.description}</div>
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'20px', fontSize:'30px', color:'yellow'}}>{props.price} <Button sx={{backgroundColor:'green'}} variant="contained">Contained</Button></div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'10px', fontSize:'25px'}}>{props.title}</div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'10px', marginBottom:'10px', fontSize:'11px', width:'100%',textAlign: 'center' , justifyContent: 'center'}}>{props.description}</div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'10px',  fontSize:'25px', color:'yellow'}}>R${props.price} <Button sx={{backgroundColor:'#212121', marginTop:'20px'}} variant="contained">Adicionar ao Carrinho</Button></div>
       
     </div>
   )
--- src/components/promotionItems/promotionItem.tsx
+++ src/components/promotionItems/promotionItem.tsx
@@ -7,15 +7,16 @@ export default function PromotionItem(props:any){
 
 
 const img = '/_next/static/media/burguer.e74e9860.png'
+const descp = 'Dois hambúrgueres (100% carne bovina), alface americana, queijo cheddar, maionese Big Mac, cebola, picles e pão com gergelim.'
 
     return(
         <div className={styles.container}>
             <h2>PROMOÇÕES DA SEMANA</h2>
         
         <div className={styles.offerItems}>
-            <Card image={burguer} title={'BIG MC'} description={'Melhor Lanche do Ferra!'} price={'20,00'}/>
-            <Card image={burguer} title={'BIG MC'} description={'Melhor Lanche do Ferra!'} price={'20,00'}/>
-            <Card image={burguer} title={'BIG MC'} description={'Melhor Lanche do Ferra!'} price={'20,00'}/>
+            <Card image={burguer} title={'BIG MC'} description={descp} price={'20,00'}/>
+            <Card image={burguer} title={'BIG MC'} description={descp} price={'20,00'}/>
+            <Card image={burguer} title={'BIG MC'} description={descp} price={'20,00'}/>
             
             
         </div>
